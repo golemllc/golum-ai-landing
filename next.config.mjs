f65ea74d0a113d2af37adb3f4a1addb598dd192f
golemllc <golemainc@gmail.com>
2026-01-20 01:31:05 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",  // <=== Enables static HTML export
+  basePath: "/golemlpage", // <=== Required for GitHub Pages (unless you use a custom domain)
+  images: {
+    unoptimized: true, // <=== Required because GitHub Pages cannot optimize images on the fly
+  },
+};
+
 export default nextConfig;
