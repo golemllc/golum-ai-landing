5a4be3eb1a8732213de41ee03ad913282429469a
golemllc <golemainc@gmail.com>
2026-01-24 18:45:46 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== Enables static HTML export
-  basePath: "/golum-ai-landing", // <=== Required for GitHub Pages (unless you use a custom domain)
+  basePath: "/golem-ai-landingp", // <=== Required for GitHub Pages (unless you use a custom domain)
   images: {
     unoptimized: true, // <=== Required because GitHub Pages cannot optimize images on the fly
   },
